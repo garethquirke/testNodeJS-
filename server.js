93cb5a42ea692795c5c9fc55f41dba36a8ce6d40
garethquirke <gareth.quirke@gmail.com>
2016-12-09 14:51:43 +0000

    basic server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var config = require('./config.json');
+
+// create server to handle requests
+// express app allows routes
+var app = express();
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+server.listen(config.port, config.hostname, () => {
+    console.log('server is running at http://' + config.hostname + ':' + config.port + '/');
+});
+
+app.get('/', function (req, res) {
+    res.end('Hello World\n');
+});
